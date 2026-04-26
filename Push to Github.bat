01ec3a0cdafa0aab9c0a8773f0285341c9d9855a
Tan Hui Yang <btflutter@gmail.com>
2026-04-27 03:47:33 +0800

    Updates

--- Push to Github.bat
+++ Push to Github.bat
@@ -1,9 +1,18 @@
 @echo off
 cd /d C:\Users\bston\Dropbox\- SG Pools\sg-4d-json-main
+
+:: create/overwrite .gitignore
+echo 4d.json>.gitignore
+echo *.xls>>.gitignore
+echo *.xlsx>>.gitignore
+echo *.xlsm>>.gitignore
+echo *.csv>>.gitignore
+
 git init
+git rm --cached 4d.json *.xls *.xlsx *.xlsm *.csv 2>nul
 git add .
-git status
 git commit -m "Updates"
-git remote set-url origin https://github.com/apiusage/sg-4d-json.git
+git remote set-url origin https://github.com/apiusage/sg-4d-json.git 2>nul
 git push origin main --force
+
 pause
