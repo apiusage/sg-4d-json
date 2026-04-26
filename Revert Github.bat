986e6b9ac1f98210a3a3503fb58fcde3076b3343
Tan Hui Yang <btflutter@gmail.com>
2026-04-27 03:05:51 +0800

    Updates

--- Revert Github.bat
+++ Revert Github.bat
@@ -1,21 +1,10 @@
 @echo off
 cd /d "C:\Users\bston\Dropbox\- SG Pools\sg-4d-json-main"
 
-:: Revert to previous commit
-git reset --hard HEAD~3
+:: Reset to before 9PM yesterday
+git reset --hard $(git rev-list -1 --before="2026-04-26 21:00" main)
 
-:: Add and commit again
-git add .
-git commit -m "Updates"
-
-:: Ensure branch is main
-git branch -M main
-
-:: Reset remote
-git remote remove origin 2>nul
-git remote add origin https://github.com/apiusage/sg-4d-json.git
-
-:: Force push
-git push -u origin main --force
+:: Push
+git push origin main --force
 
 pause
